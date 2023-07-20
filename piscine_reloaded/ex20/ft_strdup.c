89f10919da11b647d3914be9cb01fcae36561665
pmelo-ca <pedromelocf@yahoo.com>
2023-07-20 20:11:17 -0300

    Ex 20 in progress

--- piscine_reloaded/ex20/ft_strdup.c
+++ piscine_reloaded/ex20/ft_strdup.c
@@ -6,21 +6,30 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 17:28:03 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/07/19 17:31:19 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/07/20 20:09:00 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-char *ft_strdup(char *src)
+char	*ft_strdup(char *src)
 {
+	char *dest;
+	int	i;
 	
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (&dest);
 }
 
-int main(void)
+int	main(void)
 {
 	char *src;
 
-	src = "Hell yes!"; 
+	src = "Hell yes!";
 	ft_strdup(src);
 }
